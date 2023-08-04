8d8ed3fd0b6020cee0f4fbadf2df1c5e61a44a3f
ShashankSheke11 <shashankshelke0619@gmail.com>
2023-08-04 17:03:53 +0530

    [refactor]: Input component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import InputField from './components/InputField'
 
 function App() {
   return (
     <div >
       <h3>CAPSTONE PROJECT</h3>
+    
+      <InputField label="Enter First Name " type="text"  placeholder='First Name' name='Name1'l/>
+
     </div>
   );
 }
